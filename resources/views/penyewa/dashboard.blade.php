5ed4d2f1688014f1d8f21c194a3e9e391533f9e9
sitiazizaah238 <sitiazizah28035@gmail.com>
2026-02-19 19:54:44 +0700

    benerin penyewa

--- resources/views/penyewa/dashboard.blade.php
+++ resources/views/penyewa/dashboard.blade.php
@@ -32,7 +32,7 @@
             {{-- ================= CONTENT ================= --}}
             <div class="p-4" style="background:#f5f7fb;min-height:100vh;">
 
-                <h3 class="fw-bold">Dashboard Penyewa</h3>
+                 <h3 class="fw-bold" style="font-size: 30px;">Dashboard Penyewa</h3>
                 <small class="text-muted">Halo! Selamat Datang {{ Auth::user()->name }}</small>
 
                 {{-- ================= CARD STATISTIK ================= --}}
